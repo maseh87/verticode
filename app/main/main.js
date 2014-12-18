808f7c28eb59d343001c3fda3cd1873ca7176ac6
Mason Hargrove <maseh87@gmail.com>
2014-12-17 17:00:04 -0800

    added auth0

--- app/main/main.js
+++ app/main/main.js
@@ -1,7 +1,33 @@
 angular.module('verticode', [
-  'lumx'
+  'lumx',
+  'auth0',
+  'angular-storage',
+  'angular-jwt'
 ])
 
-.controller('controller', ['$scope', function ($scope) {
-    $scope.test = 'test';
-}]);
+.config(['authProvider', function (authProvider) {
+  authProvider.init({
+    domain: '33m.auth0.com',
+    clientID: 'ORkiU7BYEfMQd2MvLMGmUzzXSuax84Gv'
+  });
+}])
+
+.run(['auth', function (auth) {
+  // This hooks al auth events to check everything as soon as the app starts
+  auth.hookEvents();
+
+}])
+
+.controller('LoginCtrl', ['$scope', 'store', '$location', 'auth', function ($scope, store, $location, auth) {
+  $scope.login = function() {
+    auth.signin({}, function(profile, token) {
+      // Success callback
+      store.set('profile', profile);
+      store.set('token', token);
+      $location.path('/');
+    }, function() {
+      // Error callback
+      console.log('Error in Login controller');
+    });
+  }
+}])
