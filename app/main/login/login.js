2e5cd685197328f9d4f31cd9e38c64d2abcb6227
Mason Hargrove <maseh87@gmail.com>
2014-12-20 14:57:22 -0800

    added auth

--- app/main/login/login.js
+++ app/main/login/login.js
@@ -1,22 +1,22 @@
 angular.module('main.login', [])
 
 .config(['$stateProvider', function($stateProvider) {
-	$stateProvider
-		.state('main.login', {
-			url: 'login',
-			templateUrl: '<ui-view></ui-view>',
-			controller: 'LoginCtrl'
-		});
+  $stateProvider
+    .state('main.login', {
+      url: 'login',
+      templateUrl: '<ui-view></ui-view>',
+      controller: 'LoginCtrl'
+    });
 }])
 
 .controller('LoginCtrl', ['$scope', 'AuthFactory', '$state', function ($scope, AuthFactory, $state) {
-	$scope.signin = function() {
-  	AuthFactory.login()
-    	.then(function(profile) {
-      	$state.go('main');
-    	}).catch(function(err) {
-      	console.log(err);
-    	});
- 	};
+  $scope.signin = function() {
+    AuthFactory.login()
+      .then(function(profile) {
+        $state.go('main');
+      }).catch(function(err) {
+        console.log(err);
+      });
+  };
   $scope.signin();
 }]);
